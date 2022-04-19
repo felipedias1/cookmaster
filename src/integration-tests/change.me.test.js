39f9f8db7e65051ba22b1b400b5ba9087779a0e3
Felipe Dias <felipeferreiradias1@gmail.com>
2022-04-19 08:59:16 -0300

    Feat: Create integration tests

--- src/integration-tests/change.me.test.js
+++ src/integration-tests/change.me.test.js
@@ -1,2 +1,29 @@
+const chai = require('chai');
+const sinon = require('sinon');
+
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+const {expected} = chai;
+const server = require('../api/app')
+
+const { getConnection } = require('./connectionMock') 
+const { MongoClient } = require('mongodb')
+
+const jwt = require('jsonwebtoken')
+
+let connectionMock;
+
+before( async () => {
+  connectionMock = await getConnection();
+  sinon.stub(MongoClient, 'connect').resolves(connectionMock);
+  
+})
+
+after( () => {
+  MongoClient.connect.restore();
+})
+
 describe('', () => {
 });
